db96773953969bbf09e7f36b0e2ba6b7dd168543
jeffismad-1 <fffghfdhh@gmail.com>
2024-10-24 14:30:46 +0300

    Add files via upload
    update time

--- Plugin_Develompent/src/main/java/main/plugin_Develompent/TurtleHatche.java
+++ Plugin_Develompent/src/main/java/main/plugin_Develompent/TurtleHatche.java
@@ -1,6 +1,13 @@
 package main.plugin_Develompent;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TurtleHatche  implements Listener {
@@ -9,5 +16,14 @@ public class TurtleHatche  implements Listener {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
+    @EventHandler
+    public void onturtleHatche(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        ItemStack TurtleHatche = new ItemStack(Material.NETHERITE_AXE);
+        if (pla)
+        if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            if (player.has)
+        }
+    }
 
 }
