1126b3290f75e23fc6985ebfb4e51bcc385cc83c
jeffismad-1 <fffghfdhh@gmail.com>
2024-10-24 14:50:05 +0300

    Add files via upload
    i made it so the player only can right click if it has the name

--- Plugin_Develompent/src/main/java/main/plugin_Develompent/TurtleHatche.java
+++ Plugin_Develompent/src/main/java/main/plugin_Develompent/TurtleHatche.java
@@ -1,6 +1,8 @@
 package main.plugin_Develompent;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,7 +10,11 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class TurtleHatche  implements Listener {
 
@@ -20,10 +26,31 @@ public class TurtleHatche  implements Listener {
     public void onturtleHatche(PlayerInteractEvent event) {
         Player player = event.getPlayer();
         ItemStack TurtleHatche = new ItemStack(Material.NETHERITE_AXE);
-        if (pla)
+        ItemMeta meta = TurtleHatche.getItemMeta();
+        meta.setDisplayName(ChatColor.GOLD + " Turtle Hatcher");
+        meta.setUnbreakable(true);
+        meta.setLore(Arrays.asList(
+                ChatColor.GREEN + "Right click for a Turtle"
+                )
+        );
+
         if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            if (player.has)
+            if (player.getInventory().getItemInMainHand().equals(TurtleHatche)) {
+
+            }
         }
     }
 
+    private boolean playerHasItem(Player player, ItemStack TurtleHatche) {
+        for (ItemStack item : player.getInventory().getContents()) {
+            if (item != null && item.isSimilar(TurtleHatche)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
+
+
+
