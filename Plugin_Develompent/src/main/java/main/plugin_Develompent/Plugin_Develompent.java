4fb05f95ecb245b7daa14083aaf4e00f8031a506
jeffismad-1 <fffghfdhh@gmail.com>
2024-10-24 14:11:12 +0300

    Add files via upload

---
+++ Plugin_Develompent/src/main/java/main/plugin_Develompent/Plugin_Develompent.java
@@ -0,0 +1,17 @@
+package main.plugin_Develompent;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Plugin_Develompent extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
